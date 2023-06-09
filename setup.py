5242701ed16ed289a1a9fe6229a397765d59069c
alexrobl <vandermicra@hotmail.com>
2023-06-09 10:30:34 -0500

    estructura del flujo de ciencia de datos OOVS 2023

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Este es el repositorio principal del OOVS, en este encuentra todos los flujos de captura, conexiones a datos y modelos de inteligencia artificial propuestos por el equipo o encargados del OOVS',
+    author='Alexander EMB',
+    license='MIT',
+)
