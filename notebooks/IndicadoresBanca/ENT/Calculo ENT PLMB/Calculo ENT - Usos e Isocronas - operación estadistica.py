5a67bf1e0b5deb412a8b847437622aa2633958f0
alexrobl <alexrobl>
2023-10-11 16:25:43 +0000

    commit procesamiento octubre
    Se actualiza el repositorio con el procesamiento realizado en el mes de septiembre.

--- notebooks/IndicadoresBanca/ENT/Calculo ENT PLMB/Calculo ENT - Usos e Isocronas - operación estadistica.py
+++ notebooks/IndicadoresBanca/ENT/Calculo ENT PLMB/Calculo ENT - Usos e Isocronas - operación estadistica.py
@@ -11,16 +11,57 @@
 
 from pyspark.sql import SparkSession
 from pyspark import StorageLevel
+from sedona.spark import *
 from sedona.utils import SedonaKryoRegistrator, KryoSerializer
 
 from sedona.register.geo_registrator import SedonaRegistrator
 
+# configuración de spark y Sedona para datos geograficos
 spark = SparkSession.\
     builder.\
     master("local[*]").\
     appName("Sedona App").\
     config("spark.serializer", KryoSerializer.getName).\
     config("spark.kryo.registrator", SedonaKryoRegistrator.getName) .\
+    config("spark.kryoserializer.buffer.max", "150000m").\
     getOrCreate()
+    #config("sedona.global.charset", "utf8").\
+    
+#SedonaRegistrator.registerAll()
+SedonaContext.create(spark)
 
-SedonaRegistrator.registerAll(spark)
+# COMMAND ----------
+
+lotes_catastro = spark.sql(
+      "SELECT * from lotes_catastro"
+)
+estratos = spark.sql(
+      "SELECT * from estrato_socioeconomico"
+)
+
+
+# COMMAND ----------
+
+e = estratos.toPandas()
+e.ESoCLote.value_counts()
+
+# COMMAND ----------
+
+e
+
+# COMMAND ----------
+
+lotes = lotes_catastro.toPandas()
+
+# COMMAND ----------
+
+lotes.LOTLOTE_ID.value_counts()
+
+# COMMAND ----------
+
+[a.path for a in dbutils.fs.ls("/FileStore/tables/Catastro/union_catastro_2006_2023.csv/")]
+
+# COMMAND ----------
+
+import pandas as pd
+df = pd.read_csv('/dbfs/FileStore/tables/union_catastro_2006_2023.csv')
